48911c433642b72a86ee53af0e5ed40348d74119
Corey Lux <50592788+CLux17@users.noreply.github.com>
2020-03-12 19:26:17 +0000

    Add project files.

---
+++ OopDetails/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OopDetails
+{
+    public class Person
+    {
+        public Person()
+        {
+
+        }
+        public Person(decimal initialCreditLimit)
+        {
+            CreditLimit = initialCreditLimit;
+        }
+        public string Name { get; set; }
+        public decimal CreditLimit { get; private set; } = 1000;
+
+        public void IncreaseCreditLimit(decimal amountOfIncrease)
+        {
+            CreditLimit += amountOfIncrease;
+        }
+
+        public void SaveToDatabase(bool makeBackup)
+        {
+
+        }
+    }
+}
